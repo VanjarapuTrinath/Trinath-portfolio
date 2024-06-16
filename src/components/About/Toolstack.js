4df09997237fc8e3873f9e9e6ea706a6f294699b
VanjarapuTrinath <trinathvanjarapu3@gmail.com>
2024-06-16 19:54:26 +0530

    first commit

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiSlack,
+  SiVercel,
+  SiMacos,
+  SiMysql,
+  SiLinux,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+     
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiLinux/>
+      </Col>
+      
+    </Row>
+  );
+}
+
+export default Toolstack;
